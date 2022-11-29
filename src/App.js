25268f6cbb2067e968d42c7a83b419409f25e247
Abraham Guzman <85902876+abrahamguzman@users.noreply.github.com>
2022-11-29 14:06:40 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom"
+import Contacto from "./components/Contacto";
+import Inicio from "./components/Inicio";
+import Nosotros from "./components/Nosotros";
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Navbar</h1>
+      <hr></hr>
+      <Routes>
+        <Route path="/contacto" element={<Contacto />}/>
+        <Route path="/nosotros" element={<Nosotros />}/>
+        <Route path="/" element={<Inicio />}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
